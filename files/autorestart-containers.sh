f5e0fc377f34bed13c3d6be04e32d825580ad5f3
Trevor Howard <thatrevguy@outlook.com>
2018-05-24 20:26:59 -0700

    Add option to restart a docker swarm service

--- files/autorestart-containers.sh
+++ files/autorestart-containers.sh
@@ -21,7 +21,11 @@ while true; do
     if [ "$current_hash" != "$new_hash" ]; then
         echo ">>> Restarting dockers $containers because certificate for $domain has been modified."
         IFS=','; for container in $containers; do
+          if [ "$DOCKER_SWARM" = true ]; then
+            docker service update --detach=false --force $container
+          else
             docker restart $container
+          fi
         done; unset IFS
 
         # Keep new hash version
