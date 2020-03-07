b378e71c4641480785e244c2b6a0dd4f9a4e2987
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-03-06 23:11:26 -0300

    Travis CI multiarch builds

--- files/renew.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Launch renew test"
-certbot renew \
-    -n \
-    --config-dir /etc/letsencrypt --logs-dir /etc/letsencrypt/logs --work-dir /etc/letsencrypt/work \
-    --deploy-hook deploy-hook.sh
