fb85aca615c95a68c921593c70da422a5d4f5ab7
Adrien Ferrand <ferrand.ad@gmail.com>
2020-03-16 16:29:03 +0100

    Prevent creating the config file in legacy situations

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ ! -f "${CONFIG_PATH}" ]; then
-    echo "draft: true" >> "${CONFIG_PATH}"
+# Inhibit config.yml creation if legacy config is detected
+# to avoid skipping this legacy config processing.
+if [ ! -f "/etc/letsencrypt/domains.conf" ]; then
+  if [ ! -f "${CONFIG_PATH}" ]; then
+      echo "draft: true" >> "${CONFIG_PATH}"
+  fi
 fi
 
 export PATH="${HOME}/.local/bin:${PATH}"
