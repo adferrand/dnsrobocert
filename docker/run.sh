f26e0086975d3ae27b3117a9b4040785c92ad6c7
Duncan Bellamy <32509178+a16bitsysop@users.noreply.github.com>
2020-05-04 01:08:44 +0100

    add TIMEZONE environment variable to set timezone inside the container (#104)
    Co-authored-by: Adrien Ferrand <ferrand.ad@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+if [ -n "$TIMEZONE" ]
+then
+  echo "Waiting for DNS"
+  ping -c1 -W60 google.com || ping -c1 -W60 www.google.com
+  apk add --no-cache tzdata
+  if [ -f /usr/share/zoneinfo/"$TIMEZONE" ]
+  then
+    echo "Setting timezone to $TIMEZONE"
+    cp /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
+    echo "$TIMEZONE" > /etc/timezone
+  else
+    echo "$TIMEZONE does not exist"
+  fi
+  apk del tzdata
+fi
+
 # Inhibit config.yml creation if legacy config is detected
 # to avoid skipping this legacy config processing.
 if [ ! -f "/etc/letsencrypt/domains.conf" ]; then
