16252a0fdb7849e3a185f26a0a765504d9d7341a
Adrien Ferrand <ferrand.ad@gmail.com>
2022-01-17 22:25:13 +0100

    Dynamic link to lexicon documentation

--- docs/conf.py
+++ docs/conf.py
@@ -2,18 +2,20 @@ from os.path import abspath, dirname, join
 
 import toml
 
-metadata = toml.load(join(dirname(dirname(abspath(__file__))), "pyproject.toml"))["tool"]["poetry"]
+pyproject_toml = toml.load(join(dirname(dirname(abspath(__file__))), "pyproject.toml"))
+poetry_lock = toml.load(join(dirname(dirname(abspath(__file__))), "poetry.lock"))
 
 master_doc = "index"
 project = "DNSroboCert"
-version = release = metadata["version"]
+version = release = pyproject_toml["tool"]["poetry"]["version"]
 author = "Adrien Ferrand"
-copyright = "2020, Adrien Ferrand"
+copyright = "2022, Adrien Ferrand"
 
 extensions = [
     "sphinx.ext.intersphinx",
 ]
 
+lexicon_version = [package for package in poetry_lock["package"] if package["name"] == "dns-lexicon"][0]["version"]
 intersphinx_mapping = {
-    'lexicon': ('https://dns-lexicon.readthedocs.io/en/latest/', None),
+    'lexicon': (f'https://dns-lexicon.readthedocs.io/en/{lexicon_version}/', None),
 }
