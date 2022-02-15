b1fcba95ee1f97befc67c244cbc2427ddf4d33e0
Adrien Ferrand <ferrand.ad@gmail.com>
2022-02-15 08:35:47 +0100

    Fix run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
-if [ -n "$TIMEZONE" ]
-then
-  echo "Waiting for DNS"
-  ping -c1 -W60 google.com || ping -c1 -W60 www.google.com
-  apk add --no-cache tzdata
-  if [ -f /usr/share/zoneinfo/"$TIMEZONE" ]
-  then
+if [ -n "$TIMEZONE" ]; then
+  if [ -f /usr/share/zoneinfo/"$TIMEZONE" ]; then
     echo "Setting timezone to $TIMEZONE"
     cp /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
     echo "$TIMEZONE" > /etc/timezone
   else
     echo "$TIMEZONE does not exist"
   fi
-  apk del tzdata
 fi
 
 # Inhibit config.yml creation if legacy config is detected
