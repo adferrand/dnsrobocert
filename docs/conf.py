b2d9b2f5283d993a3bb36331a12735512bb69b65
Adrien Ferrand <adrien.ferrand@arteris.com>
2023-09-19 16:54:40 +0200

    Test some configs

--- docs/conf.py
+++ docs/conf.py
@@ -21,3 +21,32 @@ intersphinx_mapping = {
 }
 
 html_theme = "sphinx_immaterial"
+
+html_theme_options = {
+    "icon": {
+        "repo": "fontawesome/brands/github",
+        "edit": "material/file-edit-outline",
+    },
+    "site_url": "https://jbms.github.io/sphinx-immaterial/",
+    "repo_url": "https://github.com/jbms/sphinx-immaterial/",
+    "repo_name": "Sphinx-Immaterial",
+    "edit_uri": "blob/main/docs",
+    "globaltoc_collapse": True,
+    "features": [
+        "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        "navigation.sections",
+        # "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+        "toc.follow",
+        "toc.sticky",
+        "content.tabs.link",
+        "announce.dismiss",
+    ],
+    "toc_title_is_page_title": True,
+}
